5ee0943ae2f67a3dc698920acd4a063f9033ff7a
junaid-tech12 <albo32180@gmail.com>
2026-09-22 13:13:50 +0500

    Add files via upload

---
+++ src/javabasic/JAVABASIC.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javabasic;
+
+/**
+ *
+ * @author HP
+ */
+public class JAVABASIC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World!");
+        System.out.println("My name is Junaid.");
+        System.out.println("I am learning Java.");
+        
+        int age = 20;
+        System.out.println("My age is: " + age);
+
+    }
+    
+}
